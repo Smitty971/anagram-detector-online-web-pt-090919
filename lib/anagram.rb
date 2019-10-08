65cc0cc068ad385a5856ede921812085cd50b258
Jamal Turner <jlsmitty2273@yahoo.com>
2019-10-08 13:45:16 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram 
+  attr_accessor :word
+  
+  def initialize (word)
+    @word = word 
+  end 
+  
+  def match (phrase)
+    grams = []
+    phrase.each { |phrase|grams << piece if piece.split.sort == @word.sort} 
+    grams
+  end 
+end
